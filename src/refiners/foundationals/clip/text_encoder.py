d8004718c8a3805951b7ae2c5bf10445b36172e5
Cédric Deltheil <cedric@deltheil.me>
2023-08-29 16:33:33 +0200

    foundationals: add clip image encoder

--- src/refiners/foundationals/clip/text_encoder.py
+++ src/refiners/foundationals/clip/text_encoder.py
@@ -1,5 +1,6 @@
-from torch import Tensor, arange, device as Device, dtype as DType
+from torch import device as Device, dtype as DType
 import refiners.fluxion.layers as fl
+from refiners.foundationals.clip.common import PositionalEncoder, FeedForward
 from refiners.foundationals.clip.tokenizer import CLIPTokenizer
 
 
@@ -20,55 +21,6 @@ class TokenEncoder(fl.Embedding):
             embedding_dim=embedding_dim,
             device=device,
             dtype=dtype,
-        )
-
-
-class PositionalEncoder(fl.Chain):
-    structural_attrs = ["max_sequence_length", "embedding_dim"]
-
-    def __init__(
-        self,
-        max_sequence_length: int,
-        embedding_dim: int,
-        device: Device | str | None = None,
-        dtype: DType | None = None,
-    ) -> None:
-        self.max_sequence_length = max_sequence_length
-        self.embedding_dim = embedding_dim
-        super().__init__(
-            fl.Lambda(func=self.get_position_ids),
-            fl.Embedding(
-                num_embeddings=max_sequence_length,
-                embedding_dim=embedding_dim,
-                device=device,
-                dtype=dtype,
-            ),
-        )
-
-    @property
-    def position_ids(self) -> Tensor:
-        return arange(end=self.max_sequence_length, device=self.device).reshape(1, -1)
-
-    def get_position_ids(self, x: Tensor) -> Tensor:
-        return self.position_ids[:, : x.shape[1]]
-
-
-class FeedForward(fl.Chain):
-    structural_attrs = ["embedding_dim", "feedforward_dim"]
-
-    def __init__(
-        self,
-        embedding_dim: int,
-        feedforward_dim: int,
-        device: Device | str | None = None,
-        dtype: DType | None = None,
-    ) -> None:
-        self.embedding_dim = embedding_dim
-        self.feedforward_dim = feedforward_dim
-        super().__init__(
-            fl.Linear(in_features=embedding_dim, out_features=feedforward_dim, device=device, dtype=dtype),
-            fl.GeLU(),
-            fl.Linear(in_features=feedforward_dim, out_features=embedding_dim, device=device, dtype=dtype),
         )
 
 
