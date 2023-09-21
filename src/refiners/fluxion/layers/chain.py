finegrain-ai/refiners

commit 1cb798e8ae9be88c9b4ee27d73ae6c1c501c91ea

remove structural_attrs

src/refiners/fluxion/layers/chain.py
@@ -44,8 +44,6 @@ def generate_unique_names(
 
 
 class UseContext(ContextModule):
-    structural_attrs = ["context", "key", "func"]
-
     def __init__(self, context: str, key: str) -> None:
         super().__init__()
         self.context = context
@@ -73,8 +71,6 @@ class SetContext(ContextModule):
     The context need to pre exist in the context provider.
     #TODO Is there a way to create the context if it doesn't exist?
     """
-
-    structural_attrs = ["context", "key", "callback"]
 
     def __init__(self, context: str, key: str, callback: Callable[[Any, Any], Any] | None = None) -> None:
         super().__init__()
@@ -374,29 +370,16 @@ class Chain(ContextModule):
 
         Such copies can be adapted without disrupting the base model, but do not
         require extra GPU memory since the weights are in the leaves and hence not copied.
-
-        This assumes all subclasses define the class variable `structural_attrs` which
-        contains a list of basic attributes set in the constructor. In complicated cases
-        it may be required to overwrite that method.
         """
         if hasattr(self, "_pre_structural_copy"):
             self._pre_structural_copy()
 
         modules = [structural_copy(m) for m in self]
-
-        # Instantiate the right subclass, but do not initialize.
-        clone = object.__new__(self.__class__)
-
-        # Copy all basic attributes of the class declared in `structural_attrs`.
-        for k in self.__class__.structural_attrs:
-            setattr(clone, k, getattr(self, k))
-
-        # Call constructor of Chain, which among other things refreshes the context tree.
-        Chain.__init__(clone, *modules)
+        clone = super().structural_copy()
+        clone._provider = ContextProvider()
 
         for module in modules:
-            if isinstance(module, ContextModule):
-                module._set_parent(clone)
+            clone.append(module=module)
 
         if hasattr(clone, "_post_structural_copy"):
             clone._post_structural_copy(self)
@@ -477,7 +460,6 @@ class Breakpoint(ContextModule):
 
 class Concatenate(Chain):
     _tag = "CAT"
-    structural_attrs = ["dim"]
 
     def __init__(self, *modules: Module, dim: int = 0) -> None:
         super().__init__(*modules)

src/refiners/fluxion/adapters/lora.py
@@ -12,8 +12,6 @@ TLoraAdapter = TypeVar("TLoraAdapter", bound="LoraAdapter[Any]")  # Self (see PE
 
 
 class Lora(fl.Chain):
-    structural_attrs = ["in_features", "out_features", "rank", "scale"]
-
     def __init__(
         self,
         in_features: int,
@@ -56,8 +54,6 @@ class Lora(fl.Chain):
 
 
 class SingleLoraAdapter(fl.Sum, Adapter[fl.Linear]):
-    structural_attrs = ["in_features", "out_features", "rank", "scale"]
-
     def __init__(
         self,
         target: fl.Linear,

src/refiners/fluxion/layers/attentions.py
@@ -82,18 +82,6 @@ class ScaledDotProductAttention(Module):
 
 
 class Attention(Chain):
-    structural_attrs = [
-        "embedding_dim",
-        "num_heads",
-        "heads_dim",
-        "key_embedding_dim",
-        "value_embedding_dim",
-        "inner_dim",
-        "use_bias",
-        "is_causal",
-        "is_optimized",
-    ]
-
     def __init__(
         self,
         embedding_dim: int,
@@ -180,8 +168,6 @@ class SelfAttention(Attention):
 
 
 class SelfAttention2d(SelfAttention):
-    structural_attrs = Attention.structural_attrs + ["channels"]
-
     def __init__(
         self,
         channels: int,

src/refiners/fluxion/layers/converter.py
@@ -17,8 +17,6 @@ class Converter(ContextModule):
         Ensure the parent module has `device` and `dtype` attributes if `set_device` or `set_dtype` are set to True.
     """
 
-    structural_attrs = ["set_device", "set_dtype"]
-
     def __init__(self, set_device: bool = True, set_dtype: bool = True) -> None:
         super().__init__()
         self.set_device = set_device

src/refiners/fluxion/layers/module.py
@@ -1,5 +1,7 @@
 from inspect import signature, Parameter
+import sys
 from pathlib import Path
+from types import ModuleType
 from typing import Any, Generator, TypeVar, TypedDict, cast
 
 from torch import device as Device, dtype as DType
@@ -96,11 +98,6 @@ class ContextModule(Module):
     _parent: "list[Chain]"
     _can_refresh_parent: bool = True  # see usage in Adapter and Chain
 
-    # Contains simple attributes set on the instance by `__init__` in subclasses
-    # and copied by `structural_copy`. Note that is not the case of `device` since
-    # Chain's __init__ takes care of it.
-    structural_attrs: list[str] = []
-
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, *kwargs)
         self._parent = []
@@ -139,9 +136,20 @@ class ContextModule(Module):
 
     def structural_copy(self: TContextModule) -> TContextModule:
         clone = object.__new__(self.__class__)
-        for k in self.__class__.structural_attrs:
+
+        not_torch_attributes = [
+            key
+            for key, value in self.__dict__.items()
+            if not key.startswith("_")
+            and isinstance(sys.modules.get(type(value).__module__), ModuleType)
+            and "torch" not in sys.modules[type(value).__module__].__name__
+        ]
+
+        for k in not_torch_attributes:
             setattr(clone, k, getattr(self, k))
-        ContextModule.__init__(clone)
+
+        ContextModule.__init__(self=clone)
+
         return clone
 
 

src/refiners/fluxion/layers/sampling.py
@@ -9,8 +9,6 @@ from torch import Tensor, Size, device as Device, dtype as DType
 
 
 class Downsample(Chain):
-    structural_attrs = ["channels", "in_channels", "out_channels", "scale_factor", "padding"]
-
     def __init__(
         self,
         channels: int,
@@ -59,8 +57,6 @@ class Interpolate(Module):
 
 
 class Upsample(Chain):
-    structural_attrs = ["channels", "upsample_factor"]
-
     def __init__(
         self,
         channels: int,

src/refiners/foundationals/clip/common.py
@@ -3,8 +3,6 @@ import refiners.fluxion.layers as fl
 
 
 class PositionalEncoder(fl.Chain):
-    structural_attrs = ["max_sequence_length", "embedding_dim"]
-
     def __init__(
         self,
         max_sequence_length: int,
@@ -33,8 +31,6 @@ class PositionalEncoder(fl.Chain):
 
 
 class FeedForward(fl.Chain):
-    structural_attrs = ["embedding_dim", "feedforward_dim"]
-
     def __init__(
         self,
         embedding_dim: int,

src/refiners/foundationals/clip/image_encoder.py
@@ -4,8 +4,6 @@ from refiners.foundationals.clip.common import PositionalEncoder, FeedForward
 
 
 class ClassEncoder(fl.Chain):
-    structural_attrs = ["embedding_dim"]
-
     def __init__(
         self,
         embedding_dim: int,
@@ -20,8 +18,6 @@ class ClassEncoder(fl.Chain):
 
 
 class PatchEncoder(fl.Chain):
-    structural_attrs = ["in_channels", "out_channels", "patch_size", "use_bias"]
-
     def __init__(
         self,
         in_channels: int,
@@ -50,8 +46,6 @@ class PatchEncoder(fl.Chain):
 
 
 class TransformerLayer(fl.Chain):
-    structural_attrs = ["embedding_dim", "feedforward_dim", "num_attention_heads", "layer_norm_eps"]
-
     def __init__(
         self,
         embedding_dim: int = 768,
@@ -80,8 +74,6 @@ class TransformerLayer(fl.Chain):
 
 
 class ViTEmbeddings(fl.Chain):
-    structural_attrs = ["image_size", "embedding_dim", "patch_size"]
-
     def __init__(
         self,
         image_size: int = 224,
@@ -90,6 +82,9 @@ class ViTEmbeddings(fl.Chain):
         device: Device | str | None = None,
         dtype: DType | None = None,
     ) -> None:
+        self.image_size = image_size
+        self.embedding_dim = embedding_dim
+        self.patch_size = patch_size
         super().__init__(
             fl.Concatenate(
                 ClassEncoder(embedding_dim=embedding_dim, device=device, dtype=dtype),
@@ -118,16 +113,6 @@ class ViTEmbeddings(fl.Chain):
 
 
 class CLIPImageEncoder(fl.Chain):
-    structural_attrs = [
-        "image_size",
-        "embedding_dim",
-        "output_dim",
-        "patch_size",
-        "num_layers",
-        "num_attention_heads",
-        "feedforward_dim",
-    ]
-
     def __init__(
         self,
         image_size: int = 224,

commit c0745ecac687119d1e4b681dacdfdbac26ace568

correctly initialize context in structural_copy
fixes a regression introduced in 1cb798e8ae9be88c9b4ee27d73ae6c1c501c91ea

src/refiners/fluxion/layers/chain.py
@@ -376,7 +376,7 @@ class Chain(ContextModule):
 
         modules = [structural_copy(m) for m in self]
         clone = super().structural_copy()
-        clone._provider = ContextProvider()
+        clone._provider = ContextProvider.create(clone.init_context())
 
         for module in modules:
             clone.append(module=module)