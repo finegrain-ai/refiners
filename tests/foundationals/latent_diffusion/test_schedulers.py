81285a76d6e0fc9f9c3240f9348db892904a2486
Cédric Deltheil <cedric@deltheil.me>
2023-10-05 16:44:38 +0200

    scheduler: add remove noise
    aka original sample prediction (or predict x0)
    
    E.g. useful for methods like self-attention guidance (see equation (2)
    in https://arxiv.org/pdf/2210.00939.pdf)

--- tests/foundationals/latent_diffusion/test_schedulers.py
+++ tests/foundationals/latent_diffusion/test_schedulers.py
@@ -71,6 +71,31 @@ def test_ddim_solver_diffusers():
         assert allclose(diffusers_output, refiners_output, rtol=0.01), f"outputs differ at step {step}"
 
 
+def test_scheduler_remove_noise():
+    from diffusers import DDIMScheduler  # type: ignore
+
+    diffusers_scheduler = DDIMScheduler(
+        beta_end=0.012,
+        beta_schedule="scaled_linear",
+        beta_start=0.00085,
+        num_train_timesteps=1000,
+        set_alpha_to_one=False,
+        steps_offset=1,
+        clip_sample=False,
+    )
+    diffusers_scheduler.set_timesteps(30)
+    refiners_scheduler = DDIM(num_inference_steps=30)
+
+    sample = randn(1, 4, 32, 32)
+    noise = randn(1, 4, 32, 32)
+
+    for step, timestep in enumerate(diffusers_scheduler.timesteps):
+        diffusers_output = cast(Tensor, diffusers_scheduler.step(noise, timestep, sample).pred_original_sample)  # type: ignore
+        refiners_output = refiners_scheduler.remove_noise(x=sample, noise=noise, step=step)
+
+        assert allclose(diffusers_output, refiners_output, rtol=0.01), f"outputs differ at step {step}"
+
+
 def test_scheduler_device(test_device: Device):
     if test_device.type == "cpu":
         warn("not running on CPU, skipping")
