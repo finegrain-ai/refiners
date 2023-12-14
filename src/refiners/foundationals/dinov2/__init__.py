6dbc5f7ad32e7b28404495b10c3eaf6b3675eff5
Laureηt <laurentfainsin@protonmail.com>
2023-12-14 15:27:56 +0000

    Update src/refiners/foundationals/dinov2/__init__.py
    Co-authored-by: Benjamin Trom <benjamintrom@gmail.com>

--- src/refiners/foundationals/dinov2/__init__.py
+++ src/refiners/foundationals/dinov2/__init__.py
@@ -13,3 +13,18 @@ from .vit import (
     ViT_small,
     ViT_tiny,
 )
+
+__all__ = [
+    "DINOv2_base",
+    "DINOv2_base_reg",
+    "DINOv2_large",
+    "DINOv2_large_reg",
+    "DINOv2_small",
+    "DINOv2_small_reg",
+    "ViT",
+    "ViT_base",
+    "ViT_large",
+    "ViT_small",
+    "ViT_tiny",
+]
+
