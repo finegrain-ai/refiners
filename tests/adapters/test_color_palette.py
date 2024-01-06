aef35dea70d5e3bad5c29e16decde359a653727d
Pierre Colle <piercus@gmail.com>
2024-01-06 23:40:26 +0100

    clean unit test

--- tests/adapters/test_color_palette.py
+++ tests/adapters/test_color_palette.py
@@ -5,8 +5,6 @@ from refiners.fluxion.adapters.color_palette import ColorPaletteEncoder, SD1Colo
 from refiners.foundationals.latent_diffusion.cross_attention import CrossAttentionBlock2d
 from refiners.foundationals.latent_diffusion.stable_diffusion_1 import SD1UNet
 
-print("yoyoyoyo")
-
 
 def test_color_palette_encoder() -> None:
     in_channels = 22
@@ -22,8 +20,7 @@ def test_color_palette_encoder() -> None:
     color_size = 4
 
     palettes = torch.zeros(batch_size, color_size, 3)
-    print("palettes.get_device()", palettes.get_device())
-
+    
     encoded = color_palette_encoder(palettes)
 
     assert isinstance(encoded.shape, torch.Size)
