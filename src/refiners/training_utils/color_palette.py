1fe3ec8ef77bfc2ab44b2f6662a48f52b4395a9f
Pierre Colle <piercus@gmail.com>
2024-01-24 18:04:13 +0100

    1k dataset experiments

--- src/refiners/training_utils/color_palette.py
+++ src/refiners/training_utils/color_palette.py
@@ -100,42 +100,50 @@ class ColorPaletteDataset(TextEmbeddingLatentsBaseDataset[TextEmbeddingColorPale
     
     def get_color_palette(self, index: int) -> ColorPalette:
         # Randomly pick a palette between 1 and 8
-        choices = range(1, 9)
-        weights = np.array([
-            getattr(self.config.color_palette.sampling_by_palette, f"palette_{i}") for i in choices
-        ])
-        sum = weights.sum()
-        probabilities = weights / sum
-        palette_index = int(random.choices(choices, probabilities, k=1)[0])
-        item = self.dataset[index]
-        return self.dataset[index][f"palette_{palette_index}"]
+        return self.dataset[index][f"palettes"][f"8"]
+        # choices = range(1, 9)
+        # weights = np.array([
+        #     getattr(self.config.color_palette.sampling_by_palette, f"palette_{i}") for i in choices
+        # ])
+        # sum = weights.sum()
+        # probabilities = weights / sum
+        # palette_index = int(random.choices(choices, probabilities, k=1)[0])
+        # item = self.dataset[index]
+        # return self.dataset[index][f"palette_{palette_index}"]
     
-    def process_caption_and_palette(self, caption: str, color_palette: Tensor) -> tuple[str, Tensor]:
+    def process_text_embedding_and_palette(self, clip_text_embedding: Tensor, color_palette: Tensor) -> tuple[Tensor, Tensor]:
         if random.random() < self.config.latent_diffusion.unconditional_sampling_probability:
             empty = color_palette[:,0:0,:]
-            return ("", empty)
+            return (self.empty_text_embedding, empty)
         if random.random() < self.config.color_palette.without_caption_probability:
-            return ("", color_palette)
-        return (caption, color_palette)
+            return (self.empty_text_embedding, color_palette)
+        return (clip_text_embedding, color_palette)
     
+    @cached_property
+    def empty_text_embedding(self):
+        return self.text_encoder("")
     
     def __getitem__(self, index: int) -> TextEmbeddingColorPaletteLatentsBatch:
-        caption = self.get_caption(index=index, caption_key=self.config.dataset.caption_key)
         color_palette = tensor([self.get_color_palette(index=index)], dtype=self.trainer.dtype)
-        image = self.get_image(index=index)
-        resized_image = self.resize_image(
-            image=image,
-            min_size=self.config.dataset.resize_image_min_size,
-            max_size=self.config.dataset.resize_image_max_size,
-        )
-        processed_image = self.process_image(resized_image)
-        latents = self.lda.encode_image(image=processed_image)
-        (processed_caption, processed_palette) = self.process_caption_and_palette(caption=caption, color_palette=color_palette)
+        # image = self.get_image(index=index)
+        # resized_image = self.resize_image(
+        #     image=image,
+        #     min_size=self.config.dataset.resize_image_min_size,
+        #     max_size=self.config.dataset.resize_image_max_size,
+        # )
+        # processed_image = self.process_image(resized_image)
+        item = self.dataset[index]
+        latents = tensor(item["latents"])
+        clip_text_embedding = tensor(item["clip_text_embedding"])
 
-        clip_text_embedding = self.text_encoder(processed_caption)
+        (processed_caption, processed_palette) = self.process_text_embedding_and_palette(clip_text_embedding=clip_text_embedding, color_palette=color_palette)
+
         color_palette_embedding = self.color_palette_encoder(processed_palette)
+        
         return TextEmbeddingColorPaletteLatentsBatch(
-            text_embeddings=clip_text_embedding, latents=latents, color_palette_embeddings=color_palette_embedding
+            text_embeddings=clip_text_embedding, 
+            latents=latents, 
+            color_palette_embeddings=color_palette_embedding
         )
 
     def collate_fn(self, batch: list[TextEmbeddingColorPaletteLatentsBatch]) -> TextEmbeddingColorPaletteLatentsBatch:
