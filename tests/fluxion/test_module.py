bca50b71f269f387252186bfc93fef57e8fe6168
Pierre Chapuis <git@catwell.info>
2024-01-29 17:33:07 +0100

    test (and fix) basic_attributes

--- tests/fluxion/test_module.py
+++ tests/fluxion/test_module.py
@@ -13,3 +13,15 @@ def test_module_get_path() -> None:
     assert chain.Sum_1.Linear_2.get_path(parent=chain.Sum_1) == "Chain.Sum_1.Linear_2"
     assert chain.Sum_1.Linear_2.get_path(parent=chain.Sum_1, top=chain.Sum_1) == "Sum.Linear_2"
     assert chain.Sum_1.get_path() == "Chain.Sum_1"
+
+
+def test_module_basic_attributes() -> None:
+    class MyModule(fl.Module):
+        def __init__(self, spam: int = 0, foo: list[str | int] = ["bar", "qux", 42]) -> None:
+            self.spam = spam
+            self.foo = foo
+            self.chunky = "bacon"
+
+    m = MyModule(spam=3995)
+    assert str(m) == "MyModule(spam=3995)"
+    assert m.basic_attributes() == {"chunky": "bacon", "foo": ["bar", "qux", 42], "spam": 3995}
