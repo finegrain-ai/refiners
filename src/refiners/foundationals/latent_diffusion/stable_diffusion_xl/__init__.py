af3a29f916c1db46339a95c3f29dab8cba7cf85e
Laurent <laurent@lagon.tech>
2024-02-02 11:12:33 +0000

    (mkdocstrings) export `SDXLAutoencoder` in `src/refiners/foundationals/latent_diffusion/stable_diffusion_xl/__init__.py`

--- src/refiners/foundationals/latent_diffusion/stable_diffusion_xl/__init__.py
+++ src/refiners/foundationals/latent_diffusion/stable_diffusion_xl/__init__.py
@@ -1,13 +1,14 @@
 from refiners.foundationals.latent_diffusion.stable_diffusion_xl.image_prompt import SDXLIPAdapter
-from refiners.foundationals.latent_diffusion.stable_diffusion_xl.model import StableDiffusion_XL
+from refiners.foundationals.latent_diffusion.stable_diffusion_xl.model import SDXLAutoencoder, StableDiffusion_XL
 from refiners.foundationals.latent_diffusion.stable_diffusion_xl.t2i_adapter import SDXLT2IAdapter
 from refiners.foundationals.latent_diffusion.stable_diffusion_xl.text_encoder import DoubleTextEncoder
 from refiners.foundationals.latent_diffusion.stable_diffusion_xl.unet import SDXLUNet
 
 __all__ = [
     "SDXLUNet",
-    "DoubleTextEncoder",
     "StableDiffusion_XL",
+    "DoubleTextEncoder",
+    "SDXLAutoencoder",
     "SDXLIPAdapter",
     "SDXLT2IAdapter",
 ]
