f04c8627db0f5036d5658984ea74fca43ed5e30b
isamu-isozaki <isamu.website@gmail.com>
2024-02-13 23:09:41 -0500

    Logging

--- src/refiners/foundationals/latent_diffusion/image_prompt.py
+++ src/refiners/foundationals/latent_diffusion/image_prompt.py
@@ -275,17 +275,19 @@ class ImageCrossAttention(fl.Chain):
                     ),
                 )
             )
-        print([*contexts,
-        fl.Chain(
-            fl.UseContext(context="ip_adapter", key="image_embedding"),
-            fl.Linear(
-                in_features=text_cross_attention.key_embedding_dim,
-                out_features=text_cross_attention.inner_dim,
-                bias=text_cross_attention.use_bias,
-                device=text_cross_attention.device,
-                dtype=text_cross_attention.dtype,
+        print([
+            *contexts,
+            fl.Chain(
+                fl.UseContext(context="ip_adapter", key="image_embedding"),
+                fl.Linear(
+                    in_features=text_cross_attention.key_embedding_dim,
+                    out_features=text_cross_attention.inner_dim,
+                    bias=text_cross_attention.use_bias,
+                    device=text_cross_attention.device,
+                    dtype=text_cross_attention.dtype,
+                ),
             ),
-        ),])
+        ])
         super().__init__(
             fl.Distribute(
                 fl.Identity(),
