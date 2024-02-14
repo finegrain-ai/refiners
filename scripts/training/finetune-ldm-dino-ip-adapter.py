ac3bcc563a6b7ea5de522a26d2b59eba201f083a
isamu-isozaki <isamu.website@gmail.com>
2024-02-14 00:16:49 -0500

    Fixed

--- scripts/training/finetune-ldm-dino-ip-adapter.py
+++ scripts/training/finetune-ldm-dino-ip-adapter.py
@@ -592,8 +592,8 @@ class AdapterLatentDiffusionTrainer(Trainer[AdapterLatentDiffusionConfig, IPBatc
             image_proj=self.image_proj,
             use_bias=self.config.adapter.use_bias,
         )
-        self.unet.requires_grad_(False)
         ip_adapter.requires_grad_(True)
+        self.unet.requires_grad_(False)
 
         ip_adapter.inject()
         ip_adapter.to(self.device, float32)
