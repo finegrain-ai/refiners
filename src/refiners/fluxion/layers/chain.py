6b4e2f4fc816496f75db37e9f2dedc71506bae6b
isamu-isozaki <isamu.website@gmail.com>
2024-02-13 22:39:52 -0500

    logging which chain

--- src/refiners/fluxion/layers/chain.py
+++ src/refiners/fluxion/layers/chain.py
@@ -93,14 +93,16 @@ class Chain(ContextModule):
         for module in modules:
             # Violating this would mean a ContextModule ends up in two chains,
             # with a single one correctly set as its parent.
-            print(module)
-            assert (
-                (not isinstance(module, ContextModule))
-                or (not module._can_refresh_parent)
-                or (module.parent is None)
-                or (module.parent == self)
-            ), f"{module.__class__.__name__} already has parent {module.parent.__class__.__name__}"
-
+            try:
+                assert (
+                    (not isinstance(module, ContextModule))
+                    or (not module._can_refresh_parent)
+                    or (module.parent is None)
+                    or (module.parent == self)
+                ), f"{module.__class__.__name__} already has parent {module.parent.__class__.__name__}"
+            except Exception as e:
+                print(modules)
+                print(e)
         self._regenerate_keys(modules)
         self._reset_context()
 
