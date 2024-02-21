5a4bc1009b5758750356a4a6bc7e285b0ee3b35d
LouisRouss <louisrousselmargez@gmail.com>
2024-02-21 21:39:09 +0100

    Adding FuyuTransformer + correction RoPE

--- src/refiners/foundationals/fuyu/fuyu.py
+++ src/refiners/foundationals/fuyu/fuyu.py
@@ -1,5 +1,73 @@
+from torch import Size, Tensor, device as Device, dtype as DType
+
 import refiners.fluxion.layers as fl
+from refiners.foundationals.clip.tokenizer import CLIPTokenizer
+from refiners.foundationals.fuyu.input_processor import InputEncoder
+from refiners.foundationals.fuyu.transformers import FuyuTransformer, FuyuTransformerLayer
 
-class Fuyu(
 
-)
+embedding_dim: int = 4_096,
+inner_dim: int | None = None,
+feedforward_dim: int = 16_384,
+num_heads: int = 64,
+norm_eps: float = 1e-6,
+base: int = 10_000,
+use_bias: bool = True,
+is_causal: bool = True,
+is_optimized: bool = True,
+device: Device | str | None = None,
+dtype: Dtype | None = None,
+
+class Fuyu_8b(fl.Chain):
+    def __init__(
+        self,
+        embedding_dim: int = 4096,
+        feedforward_dim: int = 16384,
+        inner_dim: int | None = none,
+        max_sequence_length: int = 16_384,
+        vocabulary_size: int = 262_144,
+        tokenizer: CLIPTokenizer | None = None,
+        patch_size: int = 30,
+        padding_value: int = 0,
+        num_layers: int = 36,
+        num_heads: int = 64,
+        norm_eps: float = 1e-6,
+        base: int = 10_000,
+        use_bias: bool = True,
+        is_causal: bool = True,
+        is_optimized: bool = True,
+        device: Device | str | None = None,
+        dtype: Dtype | None = None,
+
+    ) -> None:
+        super().__init__(
+            InputEncoder(
+                embedding_dim=embedding_dim,
+                max_sequence_length=max_sequence_length,
+                vocabulary_size=vocabulary_size,
+                tokenizer=tokenizer,
+                patch_size=patch_size,
+                padding_value=padding_value,
+                device=device,
+                dtype=dtype
+            ),
+            FuyuTransformer(
+                FuyuTransformerLayer(
+                    embedding_dim=embedding_dim,
+                    inner_dim=inner_dim,
+                    feedforward_dim=feedforward_dim,
+                    num_heads=num_heads,
+                    norm_eps=norm_eps,
+                    base=base,
+                    use_bias=use_bias,
+                    is_causal=is_causal,
+                    is_optimized=is_optimized,
+                    device=device,
+                    dtype=dtype,
+                )
+                for _ in range(num_layers)
+            ),
+        )
+        # Layer Norm
+        # Linear
+        # SoftMax
