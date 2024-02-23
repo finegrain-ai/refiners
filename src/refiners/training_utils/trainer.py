88f002cf605f7bb38a0902fd9486eae1caa49a5d
isamu-isozaki <isamu.website@gmail.com>
2024-02-23 09:07:26 -0500

    Attempt file fix

--- src/refiners/training_utils/trainer.py
+++ src/refiners/training_utils/trainer.py
@@ -5,31 +5,37 @@ from typing import Any, Callable, Generic, Literal, TypeVar, cast
 
 import torch
 from loguru import logger
-from torch import Tensor
-from torch import device as Device
-from torch import dtype as DType
-from torch import float16, float32, nn
+from torch import Tensor, device as Device, dtype as DType, float16, float32, nn
 from torch.autograd import backward
 from torch.cuda.amp import GradScaler, autocast
 from torch.optim import Optimizer
-from torch.optim.lr_scheduler import (CosineAnnealingLR,
-                                      CosineAnnealingWarmRestarts, CyclicLR,
-                                      ExponentialLR, LambdaLR, LRScheduler,
-                                      MultiplicativeLR, MultiStepLR,
-                                      OneCycleLR, ReduceLROnPlateau, StepLR)
+from torch.optim.lr_scheduler import (
+    CosineAnnealingLR,
+    CosineAnnealingWarmRestarts,
+    CyclicLR,
+    ExponentialLR,
+    LambdaLR,
+    LRScheduler,
+    MultiplicativeLR,
+    MultiStepLR,
+    OneCycleLR,
+    ReduceLROnPlateau,
+    StepLR,
+)
 from torch.utils.data import DataLoader, Dataset
 
 from refiners.fluxion import layers as fl
 from refiners.fluxion.utils import no_grad
 from refiners.training_utils.callback import Callback, CallbackConfig
 from refiners.training_utils.clock import ClockConfig, TrainingClock
-from refiners.training_utils.common import (compute_grad_norm,
-                                            count_learnable_parameters,
-                                            human_readable_number, scoped_seed)
-from refiners.training_utils.config import (BaseConfig, LRSchedulerType,
-                                            ModelConfig)
-from refiners.training_utils.gradient_clipping import (GradientClipping,
-                                                       GradientClippingConfig)
+from refiners.training_utils.common import (
+    compute_grad_norm,
+    count_learnable_parameters,
+    human_readable_number,
+    scoped_seed,
+)
+from refiners.training_utils.config import BaseConfig, LRSchedulerType, ModelConfig
+from refiners.training_utils.gradient_clipping import GradientClipping, GradientClippingConfig
 
 
 class WarmupScheduler(LRScheduler):
