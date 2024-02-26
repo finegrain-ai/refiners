e244581954a7b84ebd5660df5ca4c83b49458aab
isamu-isozaki <isamu.website@gmail.com>
2024-02-25 21:51:43 -0500

    Fixed so can load default weights

--- src/refiners/foundationals/latent_diffusion/image_prompt.py
+++ src/refiners/foundationals/latent_diffusion/image_prompt.py
@@ -511,7 +511,7 @@ class IPAdapter(Generic[T], fl.Chain, Adapter[T]):
                     if not k.startswith(prefix):
                         continue
                     cross_attention_weights[k[len(prefix):]] = v
-                cross_attn.load_state_dict(cross_attention_weights)
+                cross_attn.load_state_dict(cross_attention_weights, strict=False)
             if use_pooled_text_embedding:
                 pooled_text_embedding_proj_state_dict: dict[str, Tensor] = {
                     k.removeprefix("pooled_text_embedding_proj."): v
