aaa7bd3065fc077db8c7bb4de55ba05917c3f0d0
isamu-isozaki <isamu.website@gmail.com>
2024-03-12 17:13:47 -0400

    Logging

--- scripts/training/finetune-ldm-dino-ip-adapter.py
+++ scripts/training/finetune-ldm-dino-ip-adapter.py
@@ -1003,13 +1003,11 @@ class AdapterLatentDiffusionTrainer(Trainer[AdapterLatentDiffusionConfig, IPBatc
             pooled_text_embeddings = batch.pooled_text_embedding.to(self.device, dtype=self.dtype)
         div_factor = self.config.adapter.image_embedding_div_factor
         image_embeddings = batch.image_embedding.to(self.device, dtype=input_dtype)/div_factor
+        print(image_embeddings.shape, latents.shape, text_embeddings.shape)
         for i in range(batch_size):
             if self.config.adapter.use_pooled_text_embedding:
                 image_embeddings[i], text_embeddings[i], pooled_text_embeddings[i] = self.drop_latents(image_embeddings[i], text_embeddings[i], pooled_text_embeddings[i])
             else:
-                print(image_embeddings.shape)
-                print(type(image_embeddings))
-
                 image_embeddings[i], text_embeddings[i], _ = self.drop_latents(image_embeddings[i], text_embeddings[i])
         image_embeddings = self.image_proj(image_embeddings)
         # set IP embeddings context
