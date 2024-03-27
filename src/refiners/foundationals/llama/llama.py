5c6f359fb18916c0edd0f63108cd4d70191c2355
Nezar Lheimeur <nlheimeur@brainchipinc.com>
2024-03-27 23:00:59 +0100

    wip llama

---
+++ src/refiners/foundationals/llama/llama.py
@@ -0,0 +1,7 @@
+import refiners.fluxion.layers as fl
+
+class LlamaModel(fl.Chain):
+    "LLaMa 7b base model (used in the the HuggingFace 'llava-hf/llava-1.5-7b-hf' model)"
+
+    def __init__(self):
+        raise NotImplementedError("not implemented yet")
