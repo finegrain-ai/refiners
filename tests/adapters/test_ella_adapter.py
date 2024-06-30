3d5d9ae6da17cb61dfcb0525407ab397549fbd57
ily-R <arouiilyes@gmail.com>
2024-06-30 18:56:32 +0000

    ella adapter implementation. tested with sd1.5 model

---
+++ tests/adapters/test_ella_adapter.py
@@ -0,0 +1,44 @@
+import pytest
+import torch
+
+import refiners.fluxion.layers as fl
+from refiners.fluxion.utils import no_grad
+from refiners.foundationals.latent_diffusion import SD1ELLAAdapter, SD1UNet
+from refiners.foundationals.latent_diffusion.ella_adapter import ELLACrossAttentionAdapter
+
+
+def new_adapter(target: SD1UNet) -> SD1ELLAAdapter:
+    return SD1ELLAAdapter(target=target)
+
+
+@no_grad()
+@pytest.mark.parametrize("k_unet", [SD1UNet])
+def test_inject_eject(k_unet: type[SD1UNet], test_device: torch.device):
+    unet = k_unet(in_channels=4, device=test_device, dtype=torch.float16)
+    initial_repr = repr(unet)
+    adapter = new_adapter(unet)
+    assert repr(unet) == initial_repr
+    adapter.inject()
+    assert repr(unet) != initial_repr
+    adapter.eject()
+    assert repr(unet) == initial_repr
+    adapter.inject()
+    assert repr(unet) != initial_repr
+    adapter.eject()
+    assert repr(unet) == initial_repr
+
+
+@no_grad()
+@pytest.mark.parametrize("k_unet", [SD1UNet])
+def test_scale(k_unet: type[SD1UNet], test_device: torch.device):
+    unet = k_unet(in_channels=4, device=test_device, dtype=torch.float16)
+    adapter = new_adapter(unet).inject()
+
+    def predicate(m: fl.Module, p: fl.Chain) -> bool:
+        return isinstance(p, ELLACrossAttentionAdapter) and isinstance(m, fl.UseContext)
+
+    for m, _ in unet.walk(predicate):
+        assert isinstance(m, fl.UseContext)
+        assert m.context == "ella"
+        assert m.key == "latents"
+    assert len(adapter.sub_adapters) == 32
