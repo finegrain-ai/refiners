66cd0d57a1110f895640c7eb4c01c0e55783617a
Laurent <laurent@lagon.tech>
2024-07-11 13:04:06 +0000

    improve MultiDiffusion pipelines
    Co-authored-by: limiteinductive <benjamin@lagon.tech>
    Co-authored-by: Cédric Deltheil <355031+deltheil@users.noreply.github.com>

--- src/refiners/foundationals/latent_diffusion/stable_diffusion_1/multi_diffusion.py
+++ src/refiners/foundationals/latent_diffusion/stable_diffusion_1/multi_diffusion.py
@@ -1,41 +1,31 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
-from PIL import Image
 from torch import Tensor
 
 from refiners.foundationals.latent_diffusion.multi_diffusion import DiffusionTarget, MultiDiffusion
 from refiners.foundationals.latent_diffusion.stable_diffusion_1.model import (
     StableDiffusion_1,
-    StableDiffusion_1_Inpainting,
 )
 
 
-class SD1MultiDiffusion(MultiDiffusion[StableDiffusion_1, DiffusionTarget]):
-    def diffuse_target(self, x: Tensor, step: int, target: DiffusionTarget) -> Tensor:
-        return self.ldm(
+@dataclass(kw_only=True)
+class SD1DiffusionTarget(DiffusionTarget):
+    clip_text_embedding: Tensor
+    condition_scale: float = 7.0
+
+
+class SD1MultiDiffusion(MultiDiffusion[SD1DiffusionTarget]):
+    def __init__(self, sd: StableDiffusion_1) -> None:
+        self.sd = sd
+
+    def diffuse_target(self, x: Tensor, step: int, target: SD1DiffusionTarget) -> Tensor:
+        old_solver = self.sd.solver
+        self.sd.solver = target.solver
+        result = self.sd(
             x=x,
             step=step,
             clip_text_embedding=target.clip_text_embedding,
             condition_scale=target.condition_scale,
         )
-
-
-@dataclass
-class InpaintingDiffusionTarget(DiffusionTarget):
-    target_image: Image.Image = field(default_factory=lambda: Image.new(mode="RGB", size=(512, 512), color=255))
-    mask: Image.Image = field(default_factory=lambda: Image.new(mode="L", size=(512, 512), color=255))
-
-
-class SD1InpaintingMultiDiffusion(MultiDiffusion[StableDiffusion_1_Inpainting, InpaintingDiffusionTarget]):
-    def diffuse_target(self, x: Tensor, step: int, target: InpaintingDiffusionTarget) -> Tensor:
-        self.ldm.set_inpainting_conditions(
-            target_image=target.target_image,
-            mask=target.mask,
-        )
-
-        return self.ldm(
-            x=x,
-            step=step,
-            clip_text_embedding=target.clip_text_embedding,
-            condition_scale=target.condition_scale,
-        )
+        self.sd.solver = old_solver
+        return result
